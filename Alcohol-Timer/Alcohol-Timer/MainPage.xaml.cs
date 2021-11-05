27ff3fa54df5a7adbc79c554285a231ec973b219
smcgovern2 <smcgovern2@my.wctc.edu>
2021-11-05 11:15:39 -0500

    Add project files.

---
+++ Alcohol-Timer/Alcohol-Timer/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Alcohol_Timer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Clicked(object sender, System.EventArgs e)
+        {
+            DatePicker picker = Picker;
+            DateTime date = picker.Date;
+            DateTime currentDT = DateTime.Now;
+            TimeSpan ts = currentDT.Subtract(date);
+            TimeSpan drinkingAge = new TimeSpan(7671, 0, 0, 0);
+
+            if (date == null)
+            {
+                Lbl_Date.Text = "No date";
+            }
+            else
+            {
+                if(ts >= drinkingAge){
+                    Lbl_Date.Text = $"You can already drink alcohol";
+                }
+                else
+                {
+                    Lbl_Date.Text = $"You can drink alcohol in {drinkingAge.Subtract(ts).TotalDays.ToString("F0")} Days";
+                }
+            }
+        }
+    }
+}
